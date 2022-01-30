be4b375bbbed74c6040d295476bff875a8aa3f75
alex-varga14 <57467835+alex-varga14@users.noreply.github.com>
2022-01-29 20:01:31 -0700

    images last

--- clownproxy.c
+++ clownproxy.c
@@ -20,47 +20,10 @@
 #define MAX(a,b)((a>b)?a:b)
 #define MIN(a,b)((a<b)?a:b)
 
-/*
-
-	if()
-		printf("That is a JPG URl that needs specific handling!\n");
-	else printf("That URL looks fine to me!\n");
-	
-	bzero(PATH, 500); //to clear junk at the beginning of this buffer
-	for(; i < strlen(URL); i++)
-	{
-		strcat(PATH, &URL[i]); //copy out the path
-		break;
-	}
-	
-	printf("First half: %s\n", HOST); //firstHalf is the hostname
-	printf("Second half: %s\n", PATH); //secondalf is the path
-	if( baddie )
-	{
-		printf("That is a request that needs substituition!\n"); 
-		strcpy(PATH, "/~carey/CPSC441/ass1/pansage.gif");
-		strcpy(PATH, "/~carey/CPSC441/ass1/clown1.gif");
-		printf("Revised Second half: %s\n", PATH); //secondalf is the path
-	}
-	
-	//connect to the Web server using TCP and send the HTTP request
-	tcpSocket = makeTCPconn(HOST, HTTP_PORT);
-	if( tcpSocket > 0)
-		sendHTTPrequest(tcpSocket, HOST, PATH, 0, -1);
-	else printf("Failed to create TCP socket!\n");
-	
-	bzero(header, MAX_MESSAGE_SIZE);
-	headersize = 0;
-	inHeader = 1;
-	readBytes = 0;
-	
-	*/
-
 void handleText(char* resp);
 void handleHTML(char* resp);
 char* parseHost(char* request);
 int actualLength(char* content_length);
-void startProxy(struct sockaddr_in *address, int *prox_socket, int port);
 
 // Global Variables:
 int server_socket, conn_socket, client_socket;
@@ -86,7 +49,6 @@ struct parsedRequest
     int msg_length;
 };
 	
-	
 int main (int argc, char *argv[]){
 
     // address_server init variables
@@ -94,6 +56,7 @@ int main (int argc, char *argv[]){
 	char client_request[MAX_MESSAGE_SIZE];
 	char server_request[MAX_MESSAGE_SIZE];
 	
+	// structs to hold request and response data
 	struct clientRequestInfo reqInfo;
 	struct parsedRequest parsedRed;
 	
@@ -110,26 +73,39 @@ int main (int argc, char *argv[]){
 		fprintf(stderr, "ERROR: Clown Proxy needs a Port Number!\n");
 		exit(1);
 	}
+	// Initialize server sockaddr structure
+	memset(&server_address, 0, sizeof(&server_address)); 
 	port = atoi(argv[1]);
-    startProxy(&server_address, &server_socket, port);
+    server_address.sin_family = AF_INET; 
+    server_address.sin_port = htons(port);
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY); //IADDR_ANY to local IP
 
-  
+    //create server socket 
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    printf("Socket %d Created\n", server_socket);
+    if (server_socket < 0)
+	{
+       fprintf(stderr, "ERROR: Socket() call failed!\n");
+       exit(1);
+    }
+	//bind specific address and port to the end point
     if (bind(server_socket, (struct sockaddr *) &server_address, sizeof(struct sockaddr_in)) == -1){
         close(server_socket);
         fprintf(stderr, "ERROR: Bind() call failed!\n");
         exit(1);
     }
 
-    /* Listening/Accept/Main Loop */
+	// listen forever
     for( ; ; )
 	{
+		// start listenting for connections from clients
         if (listen(server_socket, 100) == -1){
             close(server_socket);
             fprintf(stderr, "ERROR: Listen() call failed!\n");
 			exit(1);
         }
 
-        /* accept connection */
+		// accept a connection
         conn_socket = accept(server_socket, NULL, NULL);
         if (conn_socket == -1){
             close(server_socket);
@@ -137,9 +113,7 @@ int main (int argc, char *argv[]){
 			exit(1);
         }
 
-        printf("accepted connection\n");
 
-        /* Receive GET request */
         parsedRed.receive_ptr = (char*)malloc(1000 * sizeof(char));
         memset(parsedRed.receive_ptr, 0, 1000);
         if (recv(conn_socket, parsedRed.receive_ptr, sizeof(char) * 1000, 0) == -1){
@@ -149,27 +123,29 @@ int main (int argc, char *argv[]){
 			exit(1);
         }
 
-        printf("recieved something........ standby\n");
-        printf("message received:\n%s", parsedRed.receive_ptr);
 
-        /* Parse GET Request */
+        //begin parse of get request
         parsedRed.GET_request = (char*)malloc(4000 * sizeof(char));
         memset(parsedRed.GET_request, 0, 4000);
         parsedRed.host = (char*)malloc(100 * sizeof(char));
         memset(parsedRed.host, 0, 100);
 
-        // Only Accept requests in the form of "GET"
-       parsedRed. header = strstr(parsedRed.receive_ptr, "GET");
+       parsedRed.header = strstr(parsedRed.receive_ptr, "GET");
         if (parsedRed.header == NULL){
             close(server_socket);
             close(conn_socket);
             fprintf(stderr, "ERROR: HTTP method invalid!\n");
 			exit(1);
         }
+		
 
         parsedRed.host = parseHost(parsedRed.receive_ptr);
+		
+		printf("HEADER: %s\n", parsedRed.header);
+		printf("HOST: %s\n", parsedRed.host);
+		printf("RECEIVE: %s\n   NOOOB\n", parsedRed.receive_ptr);
 
-        /* make sure we haven't loaded the page recently */
+        
         char* range;
         reqInfo.modified = NULL; 
         range = NULL;
@@ -189,10 +165,10 @@ int main (int argc, char *argv[]){
         }
         else{
             strcpy(parsedRed.GET_request, parsedRed.receive_ptr);
-        }
+        } 
         parsedRed.GET_request[strlen(parsedRed.GET_request)] = '\0';
 
-        printf("\n%s\n", parsedRed.GET_request);
+       // printf("\n%s\n", parsedRed.GET_request);
 
         // don't need receive pointer anymore
         free(parsedRed.receive_ptr);  
@@ -211,13 +187,9 @@ int main (int argc, char *argv[]){
             printf("get addr error: \n", getaddrinfo(parsedRed.host, "http", &hints, &res));
             exit(1);
         }
-        printf("got address info\n");
+        //printf("got address info\n");
 
-        /* Connection Request */
-        // loop through results and connect to the first socket we can
-        attempt_connect = res;
-        for (attempt_connect = res; attempt_connect != NULL; attempt_connect = attempt_connect->ai_next){
-            if ((client_socket = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1){
+		 if ((client_socket = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1){
                 printf("client: socket\n");
                 continue;
             }
@@ -226,21 +198,13 @@ int main (int argc, char *argv[]){
                 printf("client: connect\n");
                 continue;
             }
-            break;
-        }
 
-        if (attempt_connect == NULL){
-            close(server_socket);
-            close(conn_socket);
-            fprintf(stderr, "ERROR: socket() call failed!\n");
-			exit(1);
-        }
+        //printf("connected\n");
+		
+		printf("GETERRR: %s\n", parsedRed.GET_request);
 
-        printf("connected\n");
-
-        /* Send GET */
-        parsedRed.msg_length = strlen(parsedRed.GET_request);
-        if (send(client_socket, parsedRed.GET_request, parsedRed.msg_length, 0) == -1){
+        // send GET request
+        if (send(client_socket, parsedRed.GET_request, strlen(parsedRed.GET_request), 0) == -1){
             close(server_socket);
             close(conn_socket);
             close(client_socket);
@@ -248,11 +212,14 @@ int main (int argc, char *argv[]){
 			exit(1);
         }
 
-        /* Receive Response && Handle */
+        //receive response
         reqInfo.dest = (char*)malloc(65535 * sizeof(char));
         memset(reqInfo.dest, 0, 65535);
         parsedRed.receive_ptr = (char*)malloc(512 * sizeof(char)); // make sure this is big enough
+		printf("RECEIVE 2: %s\n", parsedRed.receive_ptr);
         memset(parsedRed.receive_ptr, 0, 512);
+		printf("RECEIVE 3: %s\n", parsedRed.receive_ptr);
+		printf("ClientReq: %s\n", parsedRed.receive_ptr);
         if (recv(client_socket, parsedRed.receive_ptr, sizeof(char) * 512, 0) == -1){
             close(server_socket);
             close(conn_socket);
@@ -260,9 +227,12 @@ int main (int argc, char *argv[]){
             fprintf(stderr, "ERROR: recv() call 2 failed!\n");
 			exit(1);
         }
+		
+		printf("RECEIVE 4: %s\n", parsedRed.receive_ptr);
+		
 
-        printf("recieved something........ standby\n");
-        printf("message received:\n%s", parsedRed.receive_ptr);
+        //printf("recieved something........ standby\n");
+        //printf("message received:\n%s", parsedRed.receive_ptr);
 
         /* Parse response and insert errors */
         char* end_of_response = NULL;
@@ -271,12 +241,19 @@ int main (int argc, char *argv[]){
         reqInfo.content_length = NULL;
 
         reqInfo.protocol = strstr(parsedRed.receive_ptr, "HTTP");
+		printf("PROTOCOL: %s\n",  reqInfo.protocol);
         reqInfo.content_type = strstr(parsedRed.receive_ptr, "Content-Type: ");
+		printf("CONTENT: %s\n",  reqInfo.content_type);
         reqInfo.content_length = strstr(parsedRed.receive_ptr, "Content-Length: ");
+		printf("CONTENT Length: %s\n",  reqInfo.content_length);
         reqInfo.body_size = actualLength(reqInfo.content_length);
+		printf("BODY SIZE: %d\n",  reqInfo.body_size);
         end_of_response = strstr(parsedRed.receive_ptr, "\r\n\r\n") + 4;
+		printf("END OF RESPONSE: %s\n",  end_of_response);
         sanity = (end_of_response - parsedRed.receive_ptr);
+		printf("SANITY: %d\n",  sanity);
         strncpy(reqInfo.dest, parsedRed.receive_ptr, sanity);
+		printf("DEST: %d\n",  sanity);
         if(strstr(reqInfo.protocol, "404 Not Found") == NULL){
             if(strstr(reqInfo.content_type, "image") == NULL){
                 strcat(reqInfo.dest, end_of_response);
@@ -294,45 +271,16 @@ int main (int argc, char *argv[]){
                     strncat(reqInfo.dest, parsedRed.receive_ptr, MIN((int)strlen(parsedRed.receive_ptr), reqInfo.body_size));
                     reqInfo.body_size -= MIN((int)strlen(parsedRed.receive_ptr), reqInfo.body_size);
                 }
-
-
-                /* Checking for html type */
-                if (strstr(reqInfo.content_type, "html") != NULL){
-                    printf("got into html\n");
-                    // this function will add the errors
+                //html
+                if (strstr(reqInfo.content_type, "html") != NULL)
                     handleHTML(reqInfo.dest);
-                } 
-
-                /* Check for Plain text type */
-                if (strstr(reqInfo.content_type, "plain") != NULL){
+                //plain text
+                if (strstr(reqInfo.content_type, "plain") != NULL)
                     handleText(reqInfo.dest);
-                }
-            }
-            else{
-                /* This is where images would be handled, but it is not working......... */
-                 memcpy(reqInfo.dest + sanity, parsedRed.receive_ptr, 512 - sanity);
-                sanity += (512 - sanity);
-                while (reqInfo.body_size > 0){
-
-                    if (recv(client_socket, parsedRed.receive_ptr, MIN(512, reqInfo.body_size), 0) == -1){
-                        close(server_socket);
-                        close(conn_socket);
-                        close(client_socket);
-                        fprintf(stderr, "ERROR: recv() call 4 failed!\n");
-						exit(1);
-                    }
-                    memcpy(reqInfo.dest + sanity, parsedRed.receive_ptr, MIN(512, reqInfo.body_size));
-                    sanity += MIN(512, reqInfo.body_size);
-                    reqInfo.body_size -= MIN((int)strlen(parsedRed.receive_ptr), reqInfo.body_size);
-                } 
-
             }
         }
 
-        printf("\n\nsending:\n%s\n", reqInfo.dest);
-
-        parsedRed.msg_length = strlen(reqInfo.dest);
-        if (send(conn_socket, reqInfo.dest, parsedRed.msg_length, 0) == -1){
+        if (send(conn_socket, reqInfo.dest, strlen(reqInfo.dest), 0) == -1){
             close(server_socket);
             close(conn_socket);
             close(client_socket);
@@ -340,12 +288,11 @@ int main (int argc, char *argv[]){
 			exit(1);
         }
 
-        /* Close superfluous sockets */
         close(client_socket);
         close(conn_socket);
 
 
-        /* deallocate memory */
+		//deallocate all memory
         if (reqInfo.modified != NULL)
             printf("%p: modified pointer\n", (void*)reqInfo.modified);{
         }
@@ -362,28 +309,13 @@ int main (int argc, char *argv[]){
         printf("%p: header pointer\n", (void*)parsedRed.header);
         parsedRed.header = NULL;
         printf("%p: host pointer\n", (void*)parsedRed.host);
-        free(parsedRed.host);
+        free(parsedRed.host); 
     }
 
     /* Terminate Connection */
     close(server_socket);
 
     return 0;
-}
-
-void startProxy(struct sockaddr_in *address, int *sockfd, int port){
-
-    memset(address, 0, sizeof(&address)); //initialize address_server to zero
-    address->sin_family = AF_INET;  //AF_INET == IPv4 protocol
-    address->sin_port = htons(port); //convert little endian to big endian
-    address->sin_addr.s_addr = htonl(INADDR_ANY); //IADDR_ANY == any local IP
-
-    *sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    printf("socket %d opened\n", *sockfd);
-    if (*sockfd < 0){
-         fprintf(stderr, "ERROR: socket() call failed!\n");
-		 exit(1);
-    }
 }
 
 char* parseHost(char* request){
